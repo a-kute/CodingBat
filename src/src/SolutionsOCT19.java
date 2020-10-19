ebf9d50b1c677b98c08c4ac2aec4b426ecad630c
atharva <akute@purdue.edu>
2020-10-19 13:04:44 -0400

    added solutions

--- src/src/SolutionsOCT19.java
+++ src/src/SolutionsOCT19.java
@@ -16,10 +16,46 @@ public class SolutionsOCT19 {
 
     }
 
+    public int last2(String str) {
+        if(str.length()<2)
+            return 0;
+        String l2 = str.substring(str.length()-2,str.length());
+        int count = 0;
+
+
+        for (int i = 0; i< str.length()-2; i++) {
+            if (str.substring(i,i+2).equals(l2)) {
+                count++;
+            }
+        }
+        return count;
+
+    }
+
+    public boolean squirrelPlay(int temp, boolean isSummer) {
+        int upperlimit = isSummer ? 100 : 90;
+        return (temp>= 60 && temp <= upperlimit);
+
+    }
+
+    public int caughtSpeeding(int speed, boolean isBirthday) {
+        int booster = isBirthday ? 5 : 0;
+        if (speed <= 60 +booster) return 0;
+        if(speed <= 80 + booster) return 1;
+        return 2;
+
+
+
+    }
+
+
+
+
+
 
     public static void main (String ... args){
         SolutionsOCT19 sol = new SolutionsOCT19();
-        String result = sol.stringSplosion("hello");
+        int result = sol.caughtSpeeding(60, false);
         System.out.println(result);
 
     }
