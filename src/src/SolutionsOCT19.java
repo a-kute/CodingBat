2cc89d57dca72daaa2830ec81fbfd51334f935f1
atharva <akute@purdue.edu>
2020-10-25 22:26:32 -0400

    added solutions OCT,25,2020

--- src/src/SolutionsOCT19.java
+++ src/src/SolutionsOCT19.java
@@ -250,6 +250,15 @@ public class SolutionsOCT19 {
 
     }
 
+    public String[] wordsFront(String[] words, int n) {
+        String[] result = new String[n];
+        for (int i = 0 ; i< n; i++) {
+            result[i] = words[i];
+        }
+        return result;
+    }
+
+
 
 
 
